f09e9e3dc35a81f437e277192356aca39b912d75
Patricio Pereira Ordenes <163659718+LukkyDuck1@users.noreply.github.com>
2026-06-29 11:23:03 -0400

    Tests de cache y config Redis; corrige serializador a JDK

--- src/main/java/cl/usm/sansaweigh/config/RedisConfig.java
+++ src/main/java/cl/usm/sansaweigh/config/RedisConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -17,7 +17,7 @@ public class RedisConfig {
         RedisTemplate<String, EspecificacionBalanza> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(new JdkSerializationRedisSerializer());
         return template;
     }
 }
